bb3e09d8cc4300758bfa89642c585f768713d169
René Sackers <rene.sackers@live.com>
2016-12-31 18:25:14 +0100

    Initial commit.

---
+++ types-gtanetwork/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="./GTANetwork.dll.d.ts" />
+/// <reference path="./GTANetworkShared.dll.d.ts" />
+/// <reference path="./System.Drawing.dll.d.ts" />
+/// <reference path="./NativeUI.dll.d.ts" />
+
+/// <reference path="./declarations.d.ts" />
