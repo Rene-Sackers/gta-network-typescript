a299a023299b38819f42fab4cbe22033ab0600cc
René Sackers <rene.sackers@live.com>
2017-01-01 22:59:23 +0100

    Added drawing.point/drawing.size imports.

--- TypeScriptProject/types-gtanetwork/declarations.d.ts
+++ TypeScriptProject/types-gtanetwork/declarations.d.ts
@@ -1,5 +1,7 @@
 declare const API: GTANetwork.Javascript.ScriptContext;
 import Keys = System.Windows.Forms.Keys;
+import Point = System.Drawing.Point;
+import Size = System.Drawing.Size;
 
 declare var resource: any;
 
