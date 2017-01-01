b8210c16d74fd66c9edb1a78d6ff738ba3163835
René Sackers <rene.sackers@live.com>
2017-01-01 18:38:49 +0100

    Added resouce variable declaration.

--- TypeScriptProject/types-gtanetwork/declarations.d.ts
+++ TypeScriptProject/types-gtanetwork/declarations.d.ts
@@ -1,6 +1,8 @@
 declare const API: GTANetwork.Javascript.ScriptContext;
 import Keys = System.Windows.Forms.Keys;
 
+declare var resource: any;
+
 declare interface IEvent<THandler> {
     connect(handler: THandler): void;
 }
